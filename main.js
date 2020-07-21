952d83a5afe6a7929d1eb8432b83617489b61646
Felipe Micheletto <felipehmicheletto@gmail.com>
2020-07-21 19:37:43 -0300

    Atualização de Dados de Perfil

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-window.setTimeout(function () {
-    const target = document.querySelectorAll('[data-anime]');
-    //console.log(target.length); 
-    target.forEach(function (element) {
-        element.classList.add('animate');
-    });
-}, 100)
+const target = document.querySelectorAll('[data-anime]');
+//console.log(target.length); 
+target.forEach(function (element) {
+    element.classList.add('animate');
+});
 
 function escurece() {
     var ps = document.querySelectorAll("p");
@@ -17,34 +15,34 @@ function escurece() {
     var back = document.getElementById("back");
     var footer = document.getElementById('footer');
 
-        ps.forEach(function (element) {
-            element.style.color = '#ffffff';
-        });
+    ps.forEach(function (element) {
+        element.style.color = '#ffffff';
+    });
 
-        h1s.forEach(function (element) {
-            element.style.color = '#ffffff';
-        });
+    h1s.forEach(function (element) {
+        element.style.color = '#ffffff';
+    });
 
-        h3s.forEach(function (element) {
-            element.style.color = '#ffffff';
-        });
+    h3s.forEach(function (element) {
+        element.style.color = '#ffffff';
+    });
 
-        hrs.forEach(function (element) {
-            element.style.borderColor = '#ffffff';
-        });
+    hrs.forEach(function (element) {
+        element.style.borderColor = '#ffffff';
+    });
 
-        cardts.forEach(function (element) {
-            element.style.background = "rgba(255, 255, 255, 0.3)";
-        });
+    cardts.forEach(function (element) {
+        element.style.background = "rgba(255, 255, 255, 0.3)";
+    });
 
-        cards.forEach(function (element) {
-            element.style.background = "rgba(255, 255, 255, 0.3)"
-        });
+    cards.forEach(function (element) {
+        element.style.background = "rgba(255, 255, 255, 0.3)"
+    });
 
-        back.style.background = '#000000'
+    back.style.background = '#000000'
 
-        footer.style.background = 'rgba(255, 255, 255, 0.2)';
+    footer.style.background = 'rgba(255, 255, 255, 0.2)';
 
-        icon.style.borderColor = "#ffffff";
+    icon.style.borderColor = "#ffffff";
 }
 
